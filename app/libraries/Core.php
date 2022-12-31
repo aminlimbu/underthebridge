4fc1a1b377d81518e720230c14f95a9422574d0a
aminlimbu <aminlimbu@htmail.com>
2022-12-31 12:09:41 +1100

    git ignore file updated; ;

--- app/libraries/Core.php
+++ app/libraries/Core.php
@@ -1,4 +1,20 @@
 <?php
+    /*
+     * Core Class
+     * Creates URL & Loads Core Controller; Route
+     */
+    class Core{
+        // properties
+        protected $currentController = 'Home';
+        protected $currentMethod = 'index';
+        protected $params = [];
 
+        // methods
+        public function __construct(){
+            // get the requested url as array
+            $url = $this->getURL();
+            
+        }
+    } 
 
 ?>
