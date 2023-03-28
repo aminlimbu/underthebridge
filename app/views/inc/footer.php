19e0d5ed499ac56f8238bb0feca5f8ccb2acbe5e
Amin Limbu <aminlimbu@hotmail.com>
2023-03-28 15:06:57 +1100

    Update footer.php
    comments added

--- app/views/inc/footer.php
+++ app/views/inc/footer.php
@@ -1,3 +1,5 @@
+<!-- Partial: Footer -->
+<!-- URLROOT is defined in /app/config/ -->
 <a id="backTop" class="backToTop" href="#top"><i class="fa fa-arrow-circle-up"></i></a>
 </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
