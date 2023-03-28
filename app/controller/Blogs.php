0b2ad7c79ad544fb194f702ab6d06306710e9dd7
Amin Limbu <aminlimbu@hotmail.com>
2023-03-28 13:23:31 +1100

    Update Blogs.php
    comments added

--- app/controller/Blogs.php
+++ app/controller/Blogs.php
@@ -6,11 +6,11 @@
         public function __construct(){
             $this->blogModel = $this->model('Blog');
         }
-
+        // index method, sets model and views
         public function index(){
             $blogs = $this->blogModel->getBlogs();
             
-            // prepare data tetrived from database
+            // prepare data retrived from database
             $data = [
                 'blogs' => $blogs
             ];
@@ -19,12 +19,13 @@
             $this->view('blogs/index', $data);
         }
 
-        // Add post
+        // Add post method
         public function addBlog(){
             if($_SERVER['REQUEST_METHOD'] == 'POST'){
-                // sanitise input
+                // sanitise input received from POST method
                 $_POST = filter_input_array(htmlspecialchars(INPUT_POST));
-
+                
+                // created associated array from data received from POST method
                 $data = [
                     'title' => trim($_POST('title')),
                     'body' => trim($_POST('body')),
@@ -37,14 +38,15 @@
 
                 // Validate title
                 if(empty($data['title'])){
-                    $data['title'] = 'Please enter title for you blog.';
+                    $data['title'] = 'Please enter title for your blog.';
                 }
 
-                // Validate body
+                // Validate body (content)
                 if(empty($data['body'])){
                     $body['body_err'] = 'Blog body cannot be empty';
                 }
-
+                
+                // populate database with new contents
                 if(empty($data['title_err']) && empty($data['title_err'])){
                     if($this->blogModel->addBlog($data)){
                         flash('blog_message', 'Blog Added');
