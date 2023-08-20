2706661f4642357ebf2d7c16b5146f6934cd97ec
Amin Limbu <aminlimbu@hotmail.com>
2023-08-20 12:33:11 +1000

    Create config.php
    created config file with env variables

---
+++ app/config/config.php
@@ -0,0 +1,19 @@
+<?
+
+// Define Global Varaibles
+    define('DB_HOST', '[localhost]');
+    define('DB_NAME', '[databaseName]');
+    define('DB_USER', '[databaseUserName]');
+    define('DB_PASS', '[databasePassword]');
+
+    define('APPROOT', dirname(dirname(__FILE__)));
+
+    // URLROOT is live URL address, 'liveUrlAddressHere' should be replaced with base URL address
+    define('URLROOT', '[liveUrlAddressHere]');
+
+    define('SITENAME', '[underthebridge]');
+
+    // replace API key from Ninja Api below
+    define('NINJAPI', '[XXNXXIXXNXXJXXAXXSXX]');
+
+?>
